8c064bb00baad15774e6da572c70b3804a23aa6a
honey <honeywarid@gmail.com>
2023-03-12 10:26:12 +0530

    utils and config files are updated.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # finance_project
+
+Create a file '.env' for defining environment variables.
+In the this file, write:
+==> MONGO_DB_URL = "<---put here the connection string --->"
--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ REMOVE_PACKAGE = "-e ."
 
 def get_requirements()->List[str]:
     with open(requriment_file_name) as requirement_file:
-        requriment_list = requirement_file.readline()
-    requriment_list = [requriment_name.replace("\n", "") for requriment_name in requriment_list]
+        requriment_list = requirement_file.readlines()
+        requriment_list = [requriment_name.replace("\n", "") for requriment_name in requriment_list]
 
-    if REMOVE_PACKAGE in requriment_list:
-        requriment_list.remove(REMOVE_PACKAGE)
-    return requriment_list
+        if REMOVE_PACKAGE in requriment_list:
+            requriment_list.remove(REMOVE_PACKAGE)
+        return requriment_list
 
 
 
@@ -23,3 +23,5 @@ setup(name='Insurance',
       packages=find_packages(),
       install_requires = get_requirements()
      )
+
+
