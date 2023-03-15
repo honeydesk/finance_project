02b35590423bfdf2914d2cf0a58768cdbf8af122
honey <honeywarid@gmail.com>
2023-03-16 01:02:09 +0530

    model pusher component

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from Insurance.components.data_validation import DataValidation
 from Insurance.components.data_transformation import DataTransformation
 from Insurance.components.model_trainer import ModelTrainer
 from Insurance.components.model_evaluation import ModelEvaluation
-
+from Insurance.components.model_pusher import ModelPusher
 
 #def test_logger_and_expection():
    # try:
@@ -55,6 +55,13 @@ if __name__=="__main__":
         model_trainer_artifact=model_trainer_artifact)
         
         model_eval_artifact = model_eval.initiate_model_evaluation()
+
+        # Model Pusher
+        model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
+        model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
+                     data_transformation_artifact= data_transformation_artifact,
+                     model_trainer_artifact= model_trainer_artifact)
+        Model_pusher_artifact = model_pusher.initiate_model_pusher()
      
      except Exception as e:
           print(e)
